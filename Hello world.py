52bbc82c6048762b18777bf7198b5192ff8d77f8
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-01 09:17:20 +0300

    Initial commit

---
+++ Hello world.py
@@ -0,0 +1,17 @@
+print("Hello World!")
+
+------------------------------------
+|                                   |
+|        Python Developer          |
+|                                   |
+|  A Comprehensive Help to         |
+|        Python development         |
+|                                  |
+|               [Bonvoiage]         |
+|                                  |
+------------------------------------
+
+
+
+
+
