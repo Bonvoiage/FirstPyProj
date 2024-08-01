3308635c4294175d02085624d362a118f1cf130e
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-01 10:43:18 +0300

    added_1_4

---
+++ Module_1/Организация программ и методы строк 1_4/01.py
@@ -0,0 +1,19 @@
+# Lesson
+# name    =   input("Введите что-то: ")
+#
+# print(type(name))
+# int(name)
+#
+# print(type(name))
+# print(name)
+#
+#
+# print(int(name))
+# print(type(name))
+#
+# name = int(name)
+# print(name)
+# print(type(name))
+
+print('anything asdas asdas asdasda' .upper().lower())                                       # до разделителя в виде " , " или " : " можно делать операции с STR
+print("Just to be clear i wanna find smth in stroke" .replace("a", "f"))         # замена буквы "a" na "f"
