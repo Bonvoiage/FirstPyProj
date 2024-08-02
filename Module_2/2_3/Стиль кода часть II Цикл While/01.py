ad01bc6a176d866bcdf0c8e629b030dd7b123dc2
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-02 09:03:38 +0300

    added 2_3

---
+++ Module_2/2_3/Стиль кода часть II Цикл While/01.py
@@ -0,0 +1,11 @@
+while True:
+    number = int(input("Number : "))
+    if number % 2 == 0:
+        print("Четное")
+        continue
+    else:
+        print("Не четное")
+        break
+
+a = 5
+# CTRL + ALT + L    
