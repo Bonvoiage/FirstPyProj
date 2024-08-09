e104cea87ad25ba3091836ed7c03fdb2825a8fc4
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-09 21:12:47 +0300

    fixed/added

--- module_4/4_1/Modules and packages/just_for_test.py
+++ module_4/4_1/Modules and packages/just_for_test.py
@@ -1,2 +1,11 @@
-from not_for_test import *
-say_hi()
+#from test_module.module_1_5 import mutable_list
+from module_2.Additional_test_module_2 import stone_range
+
+print ()
+print ()
+print ()
+
+print(isinstance(stone_range, (str, int, list, tuple)))
+
+print()
+print (stone_range)
