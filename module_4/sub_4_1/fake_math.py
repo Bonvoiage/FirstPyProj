f255b544e5b661ad77925113a0d22e9dbc411289
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-11 11:46:39 +0300

    added_4_1

---
+++ module_4/sub_4_1/fake_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def fake_divide(a, b):
+    if b == 0:
+        return "Ошибка"
+    else:
+        return a / b
---
+++ module_4/sub_4_1/module_4_1.py
@@ -0,0 +1,27 @@
+from fake_math import fake_divide as f_d
+from true_math import true_divide as t_d
+
+
+result1 = f_d(69, 3)
+result2 = f_d(3, 0)
+result3 = t_d(49, 7)
+result4 = t_d(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+
+
+# from fake_math import fake_divide
+# from true_math import true_divide
+
+
+# result1 = fake_divide(69, 3)
+# result2 = fake_divide(3, 0)
+# result3 = true_divide(49, 7)
+# result4 = true_divide(15, 0)
+# print(result1)
+# print(result2)
+# print(result3)
+# print(result4)
