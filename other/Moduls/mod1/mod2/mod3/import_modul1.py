1e0ab7bd0627bbfe442d77cbf86f39655ee7f274
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-11 11:39:47 +0300

    test/fixed

--- other/Moduls/mod1/mod2/mod3/import_modul1.py
+++ other/Moduls/mod1/mod2/mod3/import_modul1.py
@@ -1,27 +1,27 @@
-# # from mod1 import modul1
-# # from mod1.func1 import func_module1
-# #
-# #
-# #
-# # func_module1()
+# from mod1 import modul1
+from mod1.func1 import func_module1
 
 
 
-# import sys
-# import os
+func_module1()
 
-# # Указываем абсолютный путь к директории, где лежит нужный файл
-# module_path = r"D:\Urb\FirstPyProj\module_2\sub_2_3"
 
-# # Добавляем этот путь в sys.path
-# if module_path not in sys.path:
-#     sys.path.append(module_path)
 
-# # Теперь можно импортировать переменную "a" из 01.py
-# from module_2_3 import my_list
+# # import sys
+# # import os
 
-# print(my_list)
+# # # Указываем абсолютный путь к директории, где лежит нужный файл
+# # module_path = r"D:\Urb\FirstPyProj\module_2\sub_2_3"
 
-from ....mod1 import func1 as f1
+# # # Добавляем этот путь в sys.path
+# # if module_path not in sys.path:
+# #     sys.path.append(module_path)
 
-f1.func_module1()
+# # # Теперь можно импортировать переменную "a" из 01.py
+# # from module_2_3 import my_list
+
+# # print(my_list)
+
+# from ....mod1 import func1 as f1
+
+# f1.func_module1()
