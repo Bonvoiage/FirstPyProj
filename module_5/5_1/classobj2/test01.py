9f3d29ee757872d2951d8d6cadfdd04c79b2fb2a
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-24 09:43:57 +0300

    added 5_1

--- module_5/5_1/classobj2/test01.py
+++ module_5/5_1/classobj2/test01.py
@@ -8,7 +8,14 @@ class people:
         print(f'Name: {self.name}, age: {self.age}')
 
 
+    def birthday(self):
+        self.age += 1
+        print(f"Happy {self.age} birthday {self.name}!")
+
+
 den = people("DeveA", 21)
 # den.surname = "A"
 # den.say_info()
 max = people("Uran", 41)
+
+max.birthday()
