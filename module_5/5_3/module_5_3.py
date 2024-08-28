60e1b9946f101e50e421a8430c9f6afc2e6cf8a7
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-28 15:31:31 +0300

    Update module_5_3.py

--- module_5/5_3/module_5_3.py
+++ module_5/5_3/module_5_3.py
@@ -8,7 +8,8 @@ class House:
         self.number_of_floors = number_of_floors
 
 
-      
+
+
     def __str__(self):
         return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
