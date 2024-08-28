ec61e5ca880420c369d3757cbe7a1bf504d15ed3
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-08-28 15:29:38 +0300

    Update module_5_3.py

--- module_5/5_3/module_5_3.py
+++ module_5/5_3/module_5_3.py
@@ -5,31 +5,84 @@ class House:
         self.number_of_floors = 0
         self.name = name
         self.number_of_floors = number_of_floors
+
+
       
+    def __str__(self):
+        return f"Название: {self.name}, кол-во этажей: {self.number_of_floors}"
 
     def __eq__(self, other):    
-        return self.number_of_floors == other.number_of_floors        
-
+        return self.number_of_floors == other.number_of_floors
     def __lt__(self, other):    
-        return self.number_of_floors < other.number_of_floors        
-
-    def __le__(self, other):    
-        return self.number_of_floors <= other.number_of_floors        
-    
+        return self.number_of_floors < other.number_of_floors            
+    def __le__(self, other):
+        return self.number_of_floors <= other.number_of_floors
     def __gt__(self, other):    
-        return self.number_of_floors > other.number_of_floors        
-
-
+        return self.number_of_floors > other.number_of_floors
     def __ge__(self, other):    
-        return self.number_of_floors >= other.number_of_floors        
-
+        return self.number_of_floors >= other.number_of_floors
     def __ne__(self, other):    
-        return self.number_of_floors != other.number_of_floors        
+        return self.number_of_floors != other.number_of_floors
 
+    def __add__(self, other):    
+        if isinstance(other, House) and isinstance(self, int):
+            resadd = other.number_of_floors + self
+            other.number_of_floors = resadd
+            return other
+        elif isinstance(self, House) and isinstance(other, int):
+            resadd = self.number_of_floors + other
+            self.number_of_floors = resadd
+            return self
+        elif isinstance(self, int) and isinstance(other, int):
+            resadd = self + other
+            return resadd
+        elif isinstance(self, House) and isinstance(other, House):
+            resadd = self.number_of_floors + other.number_of_floors
+            return resadd
+    def __radd__(self, other):    
+        return self.__add__(self, other)
+    def __iadd__(self, other):     
+        return self.__add__(self, other)
 
 h1 = House('ЖК Эльбрус', 10)
 h2 = House('ЖК Акация', 20)    
 
+print("print h1, h2")
+print(h1)
+print(h2)
+print()
+
+
+print("сравнение: h1 == h2")
+print(h1 == h2) # __eq__
+
+print("действие: h1 + 10")
+h1 = h1 + 10 # __add__
+print("вывод после действия: h1")
+print(h1)
+print("сравнение: h1 == h2")
+print(h1 == h2)
+print()
+
+print("действие: h1 += 10")
+h1 += 10 # __iadd__
+print("вывод значения: h1")
+print(h1)
+print()
+
+print("действие: 10 + h2")
+h2 = 10 + h2 # __radd__
+print("вывод значения: h2")
+print(h2)
+print()
+
+
+print("сравнение: параметров")
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
 
 # Цель: применить знания о перегрузке арифметических операторов и операторов сравнения.
 
