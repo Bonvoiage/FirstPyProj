15527563c6b8d192146827a803964a231cb2d37e
Bonvoiage <120028890+Bonvoiage@users.noreply.github.com>
2024-09-18 15:55:18 +0300

    fixed/edited

--- module_6/6_4_pingpong/e02.py
+++ module_6/6_4_pingpong/e02.py
@@ -1,12 +1,15 @@
 import arcade
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITTLE = "Ping Pong"
+
 class Game(arcade.Window):
     def on_activate(self):
         self.clear((255, 255, 255))
-
     
 
 if __name__ == "__main__":
-    window = Game()
+    window = Game(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITTLE)
     arcade.run
 
